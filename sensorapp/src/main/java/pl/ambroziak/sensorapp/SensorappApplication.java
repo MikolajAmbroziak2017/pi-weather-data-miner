6f91e2eb3ac0fe3056d61bba6c6321d95fdb0f91
Mikolaj Ambroziak <mikolajambroziak2017@outlook.com>
2019-06-10 19:10:41 +0200

    Initial structure of project, and add sensor controller class

---
+++ sensorapp/src/main/java/pl/ambroziak/sensorapp/SensorappApplication.java
@@ -0,0 +1,13 @@
+package pl.ambroziak.sensorapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SensorappApplication.class, args);
+    }
+
+}
