68677267d7e3e98407d4b1d23d95ada68d94dc80
Mikolaj Ambroziak <mikolajambroziak2017@outlook.com>
2019-06-11 17:58:42 +0200

    first run on raspberry test

--- src/main/java/pl/ambroziak/sensorapp/Controller/DataController.java
+++ src/main/java/pl/ambroziak/sensorapp/Controller/DataController.java
@@ -1,14 +1,15 @@
 package pl.ambroziak.sensorapp.Controller;
 
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import pl.ambroziak.sensorapp.Model.Data;
 import java.util.ArrayList;
+@Component
 
 public class DataController {
     private ArrayList<Float> allTemperature;
     private ArrayList<Float> allHumidity;
     private final SensorController dht = new SensorController();
-
 public DataController() {
     }
 
--- src/main/java/pl/ambroziak/sensorapp/SensorappApplication.java
+++ src/main/java/pl/ambroziak/sensorapp/SensorappApplication.java
@@ -1,12 +1,19 @@
 package pl.ambroziak.sensorapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pl.ambroziak.sensorapp.Controller.DataController;
 import pl.ambroziak.sensorapp.Controller.SensorController;
 
 @SpringBootApplication
 public class SensorappApplication {
-// test
+
+    public void run(){
+
+    }
     public static void main(String[] args) {
         SpringApplication.run(SensorappApplication.class, args);
-}}
+        System.out.println(new DataController().getData());
+}
+}
